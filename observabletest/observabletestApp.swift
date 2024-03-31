c410d3eb410ead52878cf301a2bf766a6566e02f
Adam Carter <14096630+adamcarter93@users.noreply.github.com>
2024-03-31 22:27:40 +0100

    Initial Commit

---
+++ observabletest/observabletestApp.swift
@@ -0,0 +1,17 @@
+//
+//  observabletestApp.swift
+//  observabletest
+//
+//  Created by Adam Carter on 31/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct observabletestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
